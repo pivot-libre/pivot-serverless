b1fb516bdecc4300c14e95f3e7c61c6c30e52265
Becky Andresen <rdandresen@gmail.com>
2021-03-17 12:26:27 -0500

    removed skeleton code

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,34 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="WAHOO!!"/>
-    <h2>WEB_SERVICE_ROOT</h2>
-    <pre>{{ WEB_SERVICE_ROOT }}</pre>
-    <h2>Example response</h2>
-    <pre style="text-align:left;">{{ exampleResponse }}</pre>
+   
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  data: function() {
-    return {
-      WEB_SERVICE_ROOT: this.$config.WEB_SERVICE_ROOT,
-      exampleResponse: null
-    };
-  },
-  components: {
-    HelloWorld
-  },
-  created: function() {
-      fetch(this.WEB_SERVICE_ROOT+'election/1')
-        .then((response) => response.json())
-        .then((json) => this.exampleResponse = JSON.stringify(json, null, 4))
-        .catch(() => alert('Error retrieving example web service'));
-    }
+  components: {},
 }
 </script>
 
